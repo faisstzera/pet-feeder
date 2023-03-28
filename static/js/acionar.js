85ffd45991be21952fc709baf4d1b6f4f6a6f28b
Cesarlopes33 <49722665+Cesarlopes33@users.noreply.github.com>
2023-03-28 19:18:09 -0300

    Alteração JS CSS HTML

---
+++ static/js/acionar.js
@@ -0,0 +1,8 @@
+function on(){
+    document.getElementById("imagem").innerHTML = "<img class='img1' src='../../static/img/on.jpeg'/>";
+
+}
+function off(){
+    document.getElementById("imagem").innerHTML = "<img class='img1' src='../../static/img/off.jpeg'/>";
+
+}
